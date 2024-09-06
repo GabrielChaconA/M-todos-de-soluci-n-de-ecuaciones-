1c7cde2572770f1ba365d7a586337159aea36283
GabrielChaconA <citroustax8849@gmail.com>
2024-09-05 22:57:16 -0600

    m

---
+++ Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+import Pruebas.Prueba2;
+
+public class Main {
+
+    static Prueba2 p = new Prueba2();
+
+    public static void main(String[] args) {
+        System.out.println(p.hola());
+    }
+
+}
---
+++ Pruebas/Prueba2.java
@@ -0,0 +1,8 @@
+package Pruebas;
+
+public class Prueba2 {
+    public String hola() {
+        String hola = "suu";
+        return hola;
+    };
+}
